7fc18cc5a1e1c07185e3f7864d60ccbc5b6bb1c7
DemonTrigger0w <jdiegoavargasm@gmail.com>
2025-07-07 17:32:55 -0400

    v1

--- frontend/inventario_visual/src/App.jsx
+++ frontend/inventario_visual/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      
-      </div>
-    </>
+    <div className='inventario'>
+      <form>
+          <h3>Subir bien</h3>
+          <label htmlFor="nombre">Nombre del activo</label>
+          <input type="text" name='nombre' placeholder='Ej. Silla' />
+          <label htmlFor="imagen">Imagen</label>
+          <input type="file" name='imagen' />
+          <label htmlFor="estado">Esta operativo?</label>
+          <input type="checkbox" name="estado" />
+          <input type="button" value="Subir" />
+        </form>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>Imagen</td>
+            <td>Estado</td>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
   )
 }
 
