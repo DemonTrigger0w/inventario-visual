40d74542ad404b11d638e549fe7afc868c33f9e0
DemonTrigger0w <jdiegoavargasm@gmail.com>
2025-07-08 17:59:26 -0400

    v1

--- frontend/inventario_visual/src/App.jsx
+++ frontend/inventario_visual/src/App.jsx
@@ -5,23 +5,21 @@ import { useState } from 'react';
 function App() {
   const formData = () => {
     const [formdata, setformdata] = useState({
-      "estado": "",
-      "img": null,
-      "nombre": "",
-      "userId": 0
+      estado: "",
+      img: null,
+      nombre: "",
+      userId: 0
     })
   }
   
   const hatlechage = (e) => {
     const [name, value] = e.target;
-    setformdata = ({...formdata, [name]:value})
+    setformdata = ({...formData, [name]:value})
   }
 
   const hatlechageimg = (e) => {
-    setformdata = ({...formdata, img: e.target.file[B]})
+    setformdata = ({...formData, img: e.target.file[B]})
   }
-
-  
 
   async function subirimange() {
 
@@ -49,13 +47,13 @@ function App() {
       <form>
           <h3>Subir bien</h3>
           <label htmlFor="id">id de usuario</label>
-          <input type="text" name="userId" />
+          <input type="text" name="userId" onChange={hatlechage} />
           <label htmlFor="nombre">Nombre del activo</label>
-          <input type="text" name="nombre"  placeholder='Ej. Silla'/>
+          <input type="text" name="nombre"  placeholder='Ej. Silla' onChange={hatlechage}/>
           <label htmlFor="imagen">Imagen</label>
-          <input type="file" name="img" />
+          <input type="file" name="img" onChange={hatlechageimg}/>
           <label htmlFor="estado">Esta operativo?</label>
-          <input type="checkbox" name="estado" />
+          <input type="checkbox" name="estado" onChange={hatlechage}/>
           <input type="button" value="Subir" onClick={subirimange} />
         </form>
       <table>
